a6650ca7dcc79de9e3f009f32033d8e30331b4a1
nionu0430 <49614925+nionu0430@users.noreply.github.com>
2019-06-27 14:15:30 +0900

    리스트페이지, 등록페이지에 하단바 추가 (연결은 아직안됨) 메인페이지에서는 연결완료

---
+++ app/src/main/java/com/example/openhack/scrapPage.java
@@ -0,0 +1,14 @@
+package com.example.openhack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class scrapPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrap_page);
+    }
+}
