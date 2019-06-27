dbb491a77fc4527c5a829b85aeb283ebd10f76b4
KimHeeil <imfinethanku1@gmail.com>
2019-06-27 17:24:24 +0900

    색상 추가, 마커 추가, 탭 추가

--- app/src/main/java/com/example/openhack/MainActivity.java
+++ app/src/main/java/com/example/openhack/MainActivity.java
@@ -1,17 +1,9 @@
 package com.example.openhack;
 
-import android.app.ActionBar;
-import android.app.ActionBar.Tab;
-import android.app.ActionBar.TabListener;
 import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
-import android.location.Location;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -29,10 +21,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
 
-    Location location;
 
-    private TextView mTextMessage;
-    ActionBar actionBar;
+     private TextView mTextMessage;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -54,42 +45,24 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-      /*  actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        Tab tab = null;
-        tab = actionBar.newTab();
-        tab.setText("지도로 보기");
-        tab.setTabListener(listener);
-        actionBar.addTab(tab);
+        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1);
+        tabHost1.setup();
 
-        //두번째 Tab 객체 생성 및 ActionBar에 추가하기
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1");
+        ts1.setContent(R.id.content1);
+        ts1.setIndicator("지도로 보기");
+        tabHost1.addTab(ts1);
 
-        tab = actionBar.newTab();//ActionBar에 붇는 Tab객체 생성
-
-        tab.setText("게시글로 보기");   //Tab에 보여지는 글씨
-
-        //Tab의 선택이 변경되는 것을 인지하는 TabListener 설정(아래쪽 객체 생성 코드 참고)
-
-        tab.setTabListener(listener);
-
-        //ActionBar에 Tab 추가
-
-        actionBar.addTab(tab);*/
-        //액션바 설정하기//
-        //액션바 타이틀 변경하기
-        getSupportActionBar().setTitle("ACTIONBAR");
-        //액션바 배경색 변경
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF339999));
-        //홈버튼 표시
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        //액션바 숨기기
-        //hideActionBar();
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
+        ts2.setContent(R.id.content2) ;
+        ts2.setIndicator("게시물로 보기") ;
+        tabHost1.addTab(ts2) ;
 
         FragmentManager fragmentManager = getFragmentManager();
         MapFragment mapFragment = (MapFragment) fragmentManager
@@ -98,121 +71,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-
-      /*  Button button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), listPage.class);
-                startActivity(intent);
-            }
-
-        });
-
-        Button button2 = findViewById(R.id.button3);
-        button2.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), registerPage.class);
-                startActivity(intent);
-            }
-
-        });*/
     }
-
-    //Tab의 선택변화를 인지하는 Listener 객체 생성
-
-    //(Button의 onClickListner 처럼 생각하시면 됩니다.)
-
-    TabListener listener = new TabListener() {
-
-
-        //Tab의 선택이 벗어날 때 호출
-
-        //첫번째 파라미터 : 선택에서 벗어나는 Tab 객체
-
-        //두번째 파라미터 : Tab에 해당하는 View를 Fragment로 만들때 사용하는 트랜젝션.(여기서는 사용X)
-
-        @Override
-
-        public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-
-            // TODO Auto-generated method stub
-
-
-        }
-
-
-        //Tab이 선택될 때 호출
-
-        //첫번째 파라미터 : 선택된 Tab 객체
-
-        //두번째 파라미터 : Tab에 해당하는 View를 Fragment로 만들때 사용하는 트랜젝션.(여기서는 사용X)
-
-        @Override
-
-        public void onTabSelected(Tab tab, FragmentTransaction ft) {
-
-            // TODO Auto-generated method stub
-
-
-            //선택된 Tab객체의 위치값(왼족 처음부터 0,1,2....순으로 됨)
-
-            int position = tab.getPosition();
-
-
-            switch (position) {
-
-                case 0: //가장 왼쪽 Tab 선택(Analog)
-
-
-                    //MainActivity가 보여 줄 View를
-
-                    //res폴더>>layout폴더>>layout_tab_0.xml 로 설정
-
-                    setContentView(R.layout.layout_tab_0);
-
-                    break;
-
-
-                case 1: //두번째 Tab 선택(Digital)
-
-
-                    //MainActivity가 보여 줄 View를
-
-                    //res폴더>>layout폴더>>layout_tab_1.xml 로 설정
-
-                    setContentView(R.layout.layout_tab_1);
-
-                    break;
-
-
-            }
-
-
-        }
-
-
-        //Tab이 재 선택될 때 호출
-
-        //첫번째 파라미터 : 재 선택된 Tab 객체
-
-        //두번째 파라미터 : Tab에 해당하는 View를 Fragment로 만들때 사용하는 트랜젝션.(여기서는 사용X)
-
-        @Override
-
-        public void onTabReselected(Tab tab, FragmentTransaction ft) {
-
-            // TODO Auto-generated method stub
-
-
-        }
-
-    };
-
     @Override
     public void onMapReady(final GoogleMap map) {
 
@@ -220,15 +79,42 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         //double latitude = location.getLatitude();
         //double longitude = location.getLongitude();
         LatLng SEOUL = new LatLng(35.95, 126.97);
+        LatLng SEOUL1 = new LatLng(35.950400, 126.975391);
+
+        LatLng SEOUL2 = new LatLng(35.950800, 126.975091);
+
+        LatLng SEOUL3 = new LatLng(35.950400, 126.975891);
 
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
+        markerOptions.title("빠리바게트");
+        markerOptions.snippet("1일 단기알바");
         map.addMarker(markerOptions);
 
+        MarkerOptions markerOptions1 = new MarkerOptions();
+        markerOptions1.position(SEOUL1);
+        markerOptions1.title("베스킨 라빈스");
+        markerOptions1.snippet("3일 급구");
+        map.addMarker(markerOptions1);
+
+        MarkerOptions markerOptions2 = new MarkerOptions();
+        markerOptions2.position(SEOUL2);
+        markerOptions2.title("사무실 이전");
+        markerOptions2.snippet("3시간 구합니다.");
+        map.addMarker(markerOptions2);
+
+        MarkerOptions markerOptions3 = new MarkerOptions();
+        markerOptions3.position(SEOUL3);
+        markerOptions3.title("피시방 대타");
+        markerOptions3.snippet("28일 하루 야간 대타구해요.");
+        map.addMarker(markerOptions3);
+
+
         map.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        map.animateCamera(CameraUpdateFactory.zoomTo(10));
+        map.animateCamera(CameraUpdateFactory.zoomTo(15));
     }
 
-}
+
+
+    }
+
